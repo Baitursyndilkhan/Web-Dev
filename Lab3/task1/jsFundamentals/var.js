eb32794d622f559f666184d5f0849d52df90dfd4
Baitursyndilkhan <adilhan200721@gmail.com>
2026-02-19 10:07:49 +0500

    lab3

---
+++ Lab3/task1/jsFundamentals/var.js
@@ -0,0 +1,13 @@
+let admin;
+let name;
+
+name = "John";
+admin = name;
+alert(admin);
+
+let earth = "Earth";
+let currentUserName = "John";
+
+const BIRTHDAY = '18.04.1982'; 
+
+const AGE = someCode(BIRTHDAY);
